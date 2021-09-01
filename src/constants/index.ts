c52072c7fa3d60a1d5234f1f246d303e09cd5363
tang <1160224892@qq.com>
2021-09-01 17:56:03 +0800

    LOCKER_721_ADDRESS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -151,7 +151,7 @@ export const INDEX_NFT_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.BSC]: '0x6d31E526208c423C29e021282b3F0fE13EF9d064'
 }
 export const LOCKER_721_ADDRESS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '',
+  [ChainId.MAINNET]: '0xD0dAe06f83c3a1f86bC670DBDB6F74dE563424f8',
   [ChainId.ROPSTEN]: '0x3e8c5D4540eC1A3baeBB5671387906Caf14CaFeb',
   [ChainId.RINKEBY]: '0x49db762E1bF8240218E03Ba989499bE1aCD20784',
   [ChainId.BSC]: '0x08A9c8E8EC6fE2633c488F9F60960aEd4DA60c47'
