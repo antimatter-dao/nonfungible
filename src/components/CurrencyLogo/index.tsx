ec336c16321f9042edf1150d4c79fcb77826e27f
tang <1160224892@qq.com>
2021-09-07 21:34:04 +0800

    compatible logo

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,4 +1,5 @@
 import { Currency, ETHER, Token } from '@uniswap/sdk'
+import { useActiveWeb3React } from 'hooks'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
@@ -7,8 +8,15 @@ import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
-export const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+export const getTokenLogoURL = (address: string, symbol = '', chainId = 1) => {
+  if (chainId === 250 && symbol) {
+    return `https://assets.spookyswap.finance/tokens/${symbol}.png`
+  }
+  if (chainId === 56) {
+    return `https://pancakeswap.finance/images/tokens/${address}.png`
+  }
+  return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+}
 
 export const getAntimatterTokenLogoURL = (address: string) =>
   `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
@@ -38,18 +46,23 @@ export default function CurrencyLogo({
   style?: React.CSSProperties
 }) {
   const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
+  const { chainId } = useActiveWeb3React()
 
   const srcs: string[] = useMemo(() => {
     if (currency === ETHER) return []
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
-        return [...uriLocations, getTokenLogoURL(currency.address)]
+        return [
+          ...uriLocations,
+          getTokenLogoURL(currency.address, currency.symbol, chainId),
+          getTokenLogoURL(currency.address)
+        ]
       }
-      return [getTokenLogoURL(currency.address)]
+      return [getTokenLogoURL(currency.address, currency.symbol, chainId), getTokenLogoURL(currency.address)]
     }
     return []
-  }, [currency, uriLocations])
+  }, [chainId, currency, uriLocations])
 
   if (currency === ETHER) {
     return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />
